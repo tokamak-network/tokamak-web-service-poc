8faf1e711d2583f01b1682a2dfb4d8d291fd2261
ggs134 <inditow@gmail.com>
2020-04-06 03:14:23 +0900

    export.manager.sh updated

--- scripts/rootchain/stake/3_export.manager.sh
+++ scripts/rootchain/stake/3_export.manager.sh
@@ -1,4 +1,4 @@
 source /home/ubuntu/account.variable
 
-/home/ubuntu/plasma-evm/build/bin/geth --nousb staking getManagers manager.json \
+/home/ubuntu/plasma-evm/build/bin/geth --nousb manage-staking getManagers manager.json \
   --datadir $DATADIR2
