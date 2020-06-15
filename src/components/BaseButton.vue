cddb156b109555d04d48b0c1ba43e2fb48dbf051
cd4761 <cd4761@gmail.com>
2020-06-15 15:15:20 +0900

    operator tab

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    :class="{ 'disable': disable }"
+    class="button"
+    @click="click"
+  >
+    {{ label }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    disable: {
+      type: Boolean,
+      default: false,
+    },
+    func: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  methods: {
+    async click () {
+      if (!this.disable) {
+        await this.func();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.button {
+  width: 100%;
+  height: 100%;
+}
+
+.button:hover {
+  cursor: pointer;
+}
+
+.disable:hover {
+  cursor: not-allowed;
+}
+</style>
